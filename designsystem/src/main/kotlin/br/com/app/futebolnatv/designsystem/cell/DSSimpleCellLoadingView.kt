b27412f83d99176ab1d219062c9cb81055acaf26
Igor Faria <ifaria@bltrain.com>
2023-08-04 09:54:33 -0300

    fix loading

--- designsystem/src/main/kotlin/br/com/app/futebolnatv/designsystem/cell/DSSimpleCellLoadingView.kt
+++ designsystem/src/main/kotlin/br/com/app/futebolnatv/designsystem/cell/DSSimpleCellLoadingView.kt
@@ -6,15 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.app.futebolnatv.designsystem.core.DSTheme
@@ -36,27 +33,24 @@ fun DSSimpleCellLoadingView() {
             containerColor = DSColor.backgroundWhite,
         ),
 
-    ) {
-        Box(Modifier.padding(DSMargin.xs)) {
-            Row(
+        ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(63.dp)
+                .shimmer(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
                 modifier = Modifier
-                    .width(158.dp)
+                    .fillMaxWidth()
                     .height(63.dp)
-                    .shimmer(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier
-                        .width(158.dp)
-                        .height(18.dp)
-                        .background(DSColor.backgroundShimmerGray)
+                    .background(DSColor.backgroundShimmerGray)
 
-                )
-            }
+            )
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
--- feature/match/src/main/kotlin/br/com/app/futebolnatv/match/presentation/ui/view/MatchListLoadingCellView.kt
+++ feature/match/src/main/kotlin/br/com/app/futebolnatv/match/presentation/ui/view/MatchListLoadingCellView.kt
@@ -1,15 +1,11 @@
 package br.com.app.futebolnatv.match.presentation.ui.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,14 +15,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.app.futebolnatv.channel.domain.model.Channel
 import br.com.app.futebolnatv.designsystem.core.DSTheme
 import br.com.app.futebolnatv.designsystem.core.color.DSColor
 import br.com.app.futebolnatv.designsystem.core.dimen.DSMargin
 import br.com.app.futebolnatv.designsystem.core.dimen.DSRadiusSize
-import br.com.app.futebolnatv.match.domain.model.Championship
-import br.com.app.futebolnatv.match.domain.model.Match
-import br.com.app.futebolnatv.team.domain.model.Team
 import com.valentinilk.shimmer.shimmer
 
 @Composable
@@ -34,176 +26,30 @@ fun MatchListLoadingCellView() {
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .height(200.dp)
             .padding(vertical = DSMargin.xxs),
         shape = RoundedCornerShape(DSRadiusSize.medium),
         colors = CardDefaults.cardColors(
             containerColor = DSColor.backgroundWhite,
         ),
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+        Row(
             modifier = Modifier
-                .padding(vertical = DSMargin.xs, horizontal = DSMargin.xs)
                 .fillMaxWidth()
+                .height(200.dp)
+                .shimmer(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
+            Box(
                 modifier = Modifier
-                    .width(158.dp)
-                    .height(18.dp)
-                    .shimmer(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .background(DSColor.backgroundShimmerGray)
-                )
-            }
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .background(DSColor.backgroundShimmerGray)
 
-            Row(
-                modifier = Modifier
-                    .width(138.dp)
-                    .height(24.dp)
-                    .padding(top = DSMargin.xxxxs)
-                    .shimmer(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .background(DSColor.backgroundShimmerGray)
-                )
-            }
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = DSMargin.xxs),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center) {
-
-                    Row(
-                        modifier = Modifier
-                            .width(36.dp)
-                            .height(46.dp)
-                            .shimmer(),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                                .background(DSColor.backgroundShimmerGray)
-                        )
-                    }
-
-                    Row(
-                        modifier = Modifier
-                            .padding(top = DSMargin.xxxs)
-                            .width(36.dp)
-                            .height(16.dp)
-                            .shimmer(),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                                .background(DSColor.backgroundShimmerGray)
-                        )
-                    }
-
-                }
-                Row(
-                    modifier = Modifier
-                        .padding(horizontal = DSMargin.lg)
-                        .width(12.dp)
-                        .height(26.dp)
-                        .shimmer(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .background(DSColor.backgroundShimmerGray)
-                    )
-                }
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center) {
-                    Row(
-                        modifier = Modifier
-                            .width(36.dp)
-                            .height(46.dp)
-                            .shimmer(),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                                .background(DSColor.backgroundShimmerGray)
-                        )
-                    }
-                    Row(
-                        modifier = Modifier
-                            .padding(top = DSMargin.xxxs)
-                            .width(36.dp)
-                            .height(16.dp)
-                            .shimmer(),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                                .background(DSColor.backgroundShimmerGray)
-                        )
-                    }
-                }
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Row(
-                    modifier = Modifier
-                        .width(66.dp)
-                        .height(36.dp)
-                        .shimmer(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .background(DSColor.backgroundShimmerGray)
-                    )
-                }
-                Row(
-                    modifier = Modifier
-                        .padding(start = DSMargin.xs)
-                        .width(66.dp)
-                        .height(36.dp)
-                        .shimmer(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .background(DSColor.backgroundShimmerGray)
-                    )
-                }
-
-            }
-
-
+            )
         }
+
+
     }
 }
 
